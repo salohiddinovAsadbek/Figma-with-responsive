46685c517e348ea46d811413a372bf869631d171
salohiddinovAsadbek <mrxcapitain@gmail.com>
2024-11-17 10:09:55 +0500

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector(".burgerNft");
+const modalBtn = document.querySelector(".burgerNftButton");
+
+modalBtn.addEventListener("click", () => {
+  modal.classList.toggle("burgerNftShow");
+});
